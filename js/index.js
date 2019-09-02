5b55db77c767808b7d18b20fd1cad138e0359005
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-02 13:12:27 +0100

    Completed MVP goals

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const siteContent = {
   "nav": {
+    "nav-item-0": "Home",
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
+    "nav-item-7": "Pricing",
     "nav-img-src": "img/logo.png"
   },
   "cta": {
@@ -37,30 +39,37 @@ const siteContent = {
   },
 };
 
+let homeLink = document.createElement('a');
+homeLink.href = '#';
+let pricing = document.createElement('a');
+pricing.href = '#';
+
+let headerNav = document.querySelector('header nav');
+headerNav.prepend(homeLink);
+headerNav.appendChild(pricing);
+
 let navLinks = document.querySelectorAll('header nav a');
-navLinks.forEach((link, i) => link.textContent = siteContent.nav['nav-item-' + (i + 1)]);
+navLinks.forEach((link, i) => {
+  link.textContent = siteContent.nav['nav-item-' + i];
+  link.style.color = 'green'; 
+});
+
 let logoImg = document.getElementById('logo-img');
 logoImg.src = siteContent.nav['nav-img-src'];
-let ctaH1 = document.querySelectorAll('.cta h1');
-let ctaBtn = document.querySelectorAll('.cta button');
+let ctaH1 = document.querySelector('.cta h1');
+ctaH1.textContent = siteContent.cta.h1;
+let ctaBtn = document.querySelector('.cta button');
+ctaBtn.textContent = siteContent.cta.button;
 let ctaImg = document.getElementById('cta-img');
 ctaImg.src = siteContent.cta['cta-img-src'];
 
-let features = document.querySelector('.main-content .top-content .text-content:first-child');
-features.children[0].textContent = siteContent['main-content']['features-h4'];
-features.children[1].textContent = siteContent['main-content']['features-content'];
-let about = document.querySelector('.main-content .top-content .text-content:last-child');
-about.children[0].textContent = siteContent['main-content']['about-h4'];
-about.children[1].textContent = siteContent['main-content']['about-content'];
-let services = document.querySelector('.main-content .bottom-content .text-content:first-child');
-services.children[0].textContent = siteContent['main-content']['services-h4'];
-services.children[1].textContent = siteContent['main-content']['services-content'];
-let product = document.querySelector('.main-content .bottom-content .text-content:nth-child(2)');
-product.children[0].textContent = siteContent['main-content']['product-h4'];
-product.children[1].textContent = siteContent['main-content']['product-content'];
-let vision = document.querySelector('.main-content .bottom-content .text-content:last-child');
-vision.children[0].textContent = siteContent['main-content']['vision-h4'];
-vision.children[1].textContent = siteContent['main-content']['vision-content'];
+let textContainers = document.querySelectorAll('.main-content .text-content');
+let containerSections = ['features', 'about', 'services', 'product', 'vision'];
+textContainers.forEach((container, i) => {
+  let section = containerSections[i];
+  container.children[0].textContent = siteContent['main-content'][section + '-h4'];
+  container.children[1].textContent = siteContent['main-content'][section + '-content'];
+});
 
 let middleImg = document.getElementById('middle-img');
 middleImg.src = siteContent['main-content']['middle-img-src'];
@@ -73,3 +82,7 @@ contact.children[3].textContent = siteContent.contact.email;
 
 let footerCopyright = document.querySelector('footer p:first-child');
 footerCopyright.textContent = siteContent.footer.copyright;
+
+//stretch
+let heading4 = document.querySelectorAll('h4');
+heading4.forEach(h => h.style.color = 'blue');
